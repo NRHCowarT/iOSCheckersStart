3fe73a8a0976eaa2ce4800bb97460d196b583679
Will16 <williammcduff@Williams-MacBook-Pro-2.local>
2015-02-17 17:01:42 -0500

    checkersApp
    checkersApp

---
+++ Checkers/WelcomeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WelcomeViewController.swift
+//  Checkers
+//
+//  Created by William McDuff on 2015-02-17.
+//  Copyright (c) 2015 Cal. All rights reserved.
+//
+
+import Cocoa
+
+class WelcomeViewController: UIViewController {
+
+}
